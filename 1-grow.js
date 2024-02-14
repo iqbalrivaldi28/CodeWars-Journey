574b4345c6c0fae7a3ae7e0494deaf943b0c173d
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-02-14 21:07:04 +0700

    feat: upload github

---
+++ 1-grow.js
@@ -0,0 +1,27 @@
+//* Soal 1 diminta untuk mengklikan semua nilai yg ada dalam sebuah array
+//* [1, 2, 3, 4] maka hasilnya harus 24
+
+function grow(x) {
+  let result = x[0];
+
+  for (let i = 1; i < x.length; i++) {
+    result = result * x[i];
+  }
+
+  return result;
+}
+
+console.log(grow([1, 2, 3, 4]));
+
+//? Bisa juga gini pake arrow function biar lebih singkt
+//? NIlai hasil = accumolator
+
+function growTwo(x) {
+  const result = x.reduce((accomulator, currentValue) => {
+    return accomulator * currentValue;
+  }, 1);
+
+  return result;
+}
+
+console.log(growTwo([2, 2, 2])); //? Hasilnya 8
