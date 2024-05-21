e5ebd19e42654aa88244fa13dcac939b361cb749
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-21 14:17:40 +0700

    feat: add soal nomor 7

---
+++ 7.count-by-x.js
@@ -0,0 +1,15 @@
+//? Link soal: https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
+function countBy(x, n){
+    let z = []
+
+    for(let i = 1; i <= n; i++){
+        z.push(i * x)
+    }
+
+    return z;
+}
+
+
+console.log(countBy(1, 10));
+console.log(countBy(2, 5));
