f6274bf78d40dd5f8cc3a3be12534620c2fdf943
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-22 07:19:55 +0700

    feat: add question number ten

---
+++ 10.convert-stting-to-array.js
@@ -0,0 +1,12 @@
+//? Link soal: https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+function stringToArray(string) {
+  return string.split(' ');
+}
+
+//? Penulisan singkatnya
+const stringToArrayTwo = (string) => string.split(' ');
+
+
+console.log(stringToArray("Belajar Coding"));
+console.log(stringToArrayTwo('I Love Jakarta'))
