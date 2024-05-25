c3ed8dfddd19ebc98c94adbbb45648b12cb66770
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-25 12:47:10 +0700

    feat: add question number 26

---
+++ 26.trafficc-light.js
@@ -0,0 +1,28 @@
+//? Link soal: https://www.codewars.com/kata/58649884a1659ed6cb000072/train/javascript
+
+function updateLight(current) {
+  if (current === 'green') {
+    return 'yellow';
+  }
+
+  if (current === 'yellow') {
+    return 'red';
+  }
+
+  if (current === 'red') {
+    return 'green';
+  }
+}
+
+//? Kalau pake ternary operator
+function updateLightDua(current) {
+  return current === 'green' ? 'yellow' : current === 'yellow' ? 'red' : 'green';
+}
+
+console.log(updateLight('green'));
+console.log(updateLight('yellow'));
+console.log(updateLight('red'));
+
+console.log(updateLightDua('green'));
+console.log(updateLightDua('yellow'));
+console.log(updateLightDua('red'));
