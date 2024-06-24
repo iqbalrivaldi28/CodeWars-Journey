2cbcffd3b7b32f550ad2f230a26618d37bc39986
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-24 07:17:06 +0700

    feat: add question number 35

---
+++ 35.merge-array.js
@@ -0,0 +1,17 @@
+//? Link soal: https://www.codewars.com/kata/5899642f6e1b25935d000161/train/javascript
+
+function mergeArrays(arr1, arr2) {
+  //? Gabungin arraynya
+  let mergedArray = [...arr1, ...arr2];
+
+  //? Kita urutin haril penggabungan arraynya
+  mergedArray.sort((a, b) => a - b);
+
+  //? Kita hapus elemen yang duplikat
+  let uniqueArray = Array.from(new Set(mergedArray));
+
+  return uniqueArray;
+}
+
+
+console.log(mergeArrays([1,2,3,4], [5,6,7,8]));
