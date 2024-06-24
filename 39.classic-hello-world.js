319a9b3a0ead398dfd3d02bb482e142508a0f196
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-25 06:37:55 +0700

    feat: add question number 39

---
+++ 39.classic-hello-world.js
@@ -0,0 +1,10 @@
+//? Link soal: https://www.codewars.com/kata/57036f007fd72e3b77000023/train/javascript
+
+// Print "Hello World!" to the screen
+class Solution{
+    static main(){
+        console.log("Hello World!");
+    }
+}
+
+Solution.main("parameter1", "parameter2")
