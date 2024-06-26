49592407e987f6500bd5a49eaf25d853aa54e864
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-26 08:13:13 +0700

    feat: add question number 42

---
+++ 42.parse-int-in-age.js
@@ -0,0 +1,9 @@
+//? Link soal: https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/javascript
+
+//? Menampilkan angka pada kalimat dan dijadikan number
+
+function getAge(inputString) {
+  return parseInt(inputString[0], 10);
+}
+
+console.log(getAge('4 years old'));
