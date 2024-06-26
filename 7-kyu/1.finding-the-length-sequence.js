1dcc2263206b2e03f3b4f6b00520437c4b7fd739
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-26 08:47:30 +0700

    ref: update 7 kyu uestion 1

--- 7-kyu/1.finding-the-length-sequence.js
+++ 7-kyu/1.finding-the-length-sequence.js
@@ -1,9 +1,9 @@
 //? Link soal: https://www.codewars.com/kata/5566b0dd450172dfc4000005/train/javascript
 
 var lengthOfSequence = function (arr, n) {
-  // Hitung jumlah kemunculan angka n dalam array
-  const count = arr.filter((x) => x === n).length;
-
+   // Hitung jumlah kemunculan angka n dalam array
+  const count = arr.filter(x => x === n).length;
+  
   // Jika angka n muncul kurang dari dua kali atau lebih dari dua kali, kembalikan 0
   if (count !== 2) {
     return 0;
@@ -15,11 +15,8 @@ var lengthOfSequence = function (arr, n) {
 
   // Hitung panjang sub-array dari indeks pertama hingga indeks terakhir
   const length = lastIndex - firstIndex + 1;
-
+  
   return length;
-};
+  };
 
-console.log(lengthOfSequence([0, -3, 7, 4, 0, 3, 7, 9], 7)); // Output: 5
-console.log(lengthOfSequence([1, 2, 3, 4, 5, 6, 7], 3)); // Output: 0
-console.log(lengthOfSequence([3, 3, 3, 3, 3], 3)); // Output: 5
-console.log(lengthOfSequence([1, 2, 3, 4, 2, 5, 2], 2)); // Output: 6
+  console.log(lengthOfSequence([0, -3, 7, 4, 0, 3, 7, 9], 7)); // Output: 5
