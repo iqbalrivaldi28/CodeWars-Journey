582e89bf948410e17361c0e64b013a6815af4c5f
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-27 07:43:39 +0700

    feat: add question number 45

--- 8-kyu/44.chuck-norris-vii.js
+++ 8-kyu/44.chuck-norris-vii.js
@@ -1,5 +1,7 @@
 //? Link soal: https://www.codewars.com/kata/570669d8cb7293a2d1001473/train/javascript
 
+//? Mmebuat fungci yang mengembalikan false tanpa menuliskan false
+
 function ifChuckSaysSo(){
     return !true
 }
---
+++ 8-kyu/45.multiply.js
@@ -0,0 +1,8 @@
+//? Link soal: https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+
+//! Debugging pada soal function tidak ada kata return
+
+function multiply(a, b){
+    return a * b
+  }
+  
